4b745d89212dcf9132f7db232716685486c34bdf
RosileneFerreira <rosilenesanferr@gmail.com>
2022-03-07 18:22:40 -0300

    Variáveis

---
+++ Módulo-I-Fundamentos-do-Desenvolvimento-Web/Bloco4-Introducao-a-JavaScript-e-Logica-de-Programação/Dia01-Introducao-JavaScript/scripts-exerc01.js
@@ -0,0 +1,11 @@
+const myName = "Rosilene";
+const birthCity = "Belo Horizonte";
+let birthYear = 1982;
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
+birthYear = 2030;
+console.log (birthYear);
+birthCity = "São Paulo";
+console.log (birthCity);
+/* Ao alterar a variável "birthCity" dá uma mensagem de erro, pois essa variável é do tipo constante. */
