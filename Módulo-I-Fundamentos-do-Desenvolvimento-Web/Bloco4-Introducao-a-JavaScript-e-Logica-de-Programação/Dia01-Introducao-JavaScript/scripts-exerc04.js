53a4325e281f36c0dad3a3bb7cfbec68238a966b
RosileneFerreira <rosilenesanferr@gmail.com>
2022-03-08 11:36:02 -0300

    Operadores && (AND) e || (OR)

--- Módulo-I-Fundamentos-do-Desenvolvimento-Web/Bloco4-Introducao-a-JavaScript-e-Logica-de-Programação/Dia01-Introducao-JavaScript/scripts-exerc04.js
+++ Módulo-I-Fundamentos-do-Desenvolvimento-Web/Bloco4-Introducao-a-JavaScript-e-Logica-de-Programação/Dia01-Introducao-JavaScript/scripts-exerc04.js
@@ -1,21 +1,33 @@
 /* Operador AND && */
-const currentHour = 23;
-let message = "0";
+/* const currentHour = 23;
+let message = "";
+
 if (currentHour >= 22) {
-    message = "Não deveriámos comer nada, é hora de dormir";
+    message === "Não deveriámos comer nada, é hora de dormir";
 }
 else if (currentHour >= 18 && currentHour < 22) {
-    message = ("Rango da noite, vamos jandar :D");
+    message ==="Rango da noite, vamos jandar :D";
 }
 else if (currentHour >= 14 && currentHour < 18) {
-    message = ("Vamos fazer um bolo pro café da tarde?");
+    message === "Vamos fazer um bolo pro café da tarde?";
 }
 else if (currentHour >= 11 && currentHour < 14) {
-    message = ("Hora do almoço!!!");
+    message === "Hora do almoço!!!";
 }
 else if (currentHour >= 4 && currentHour < 11) {
-    message = ("Hmmm, cheiro de café recém passado.");
+    message === "Hmmm, cheiro de café recém passado.";
 }
 else {
-    message = ("Estou dormindo, por isso não vou comer :)");
+    message === "Estou dormindo, por isso não vou comer :)";
+} */
+
+/* Operador || */
+/* let weekDay = "sábado";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log ("Oba, mais um dia de aprendizado na Trybe >:D.");
 }
+
+else {
+    console.log ("FINALMENTE, descanso merecido UwU.")
+} */
